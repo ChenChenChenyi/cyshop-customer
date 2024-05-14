0f088eee74b5ea0b8538e03b3d474a6f9356e225
chenyi <614362234@qq.com>
2024-05-15 00:41:12 +0800

    FEAT:按照DDD的架构搭建cyshop商城项目的代码架构

---
+++ cyshop-customer-interfaces/src/main/java/com/cyshop/customer/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.cyshop.customer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname HelloController
+ * @Description TODO
+ * @Date 2024/5/14 20:52
+ * @Created by 陈义
+ */
+@RestController
+public class HelloController {
+    /**
+    * @Description:
+    * @Author: chenyi
+    * @Date: 2024/5/14
+    */
+    @GetMapping("/hello")
+    public void sayHello(){
+        System.out.println("Hello chenyi and yinxiangzhao!");
+    }
+}
