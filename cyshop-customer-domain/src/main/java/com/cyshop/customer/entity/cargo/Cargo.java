fa5d1084124d45153b9abb0326a793e377490b5f
chenyi <614362234@qq.com>
2024-05-15 21:32:44 +0800

    FEAT:修改各模块之间的POM依赖关系

---
+++ cyshop-customer-domain/src/main/java/com/cyshop/customer/entity/cargo/Cargo.java
@@ -0,0 +1,15 @@
+package com.cyshop.customer.entity.cargo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @Classname Cargo
+ * @Description TODO
+ * @Date 2024/5/15 12:23
+ * @Created by 陈义
+ */
+@Entity
+@Table(name = "cargo")
+public class Cargo {
+}
